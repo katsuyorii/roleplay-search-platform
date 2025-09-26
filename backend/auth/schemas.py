3b172323a096ba05529b156c367d9d0295622739
None None <katsuyoriiii@gmail.com>
2025-09-26 13:32:33 +0300

    Create shemas for login and response access token

--- backend/auth/schemas.py
+++ backend/auth/schemas.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel, EmailStr, Field, field_validator
 from core.validators.password import validator_password_complexity
 
 
+class AccessTokenResponseSchema(BaseModel):
+    access_token: str
+    type: str = Field(default='Bearer')
+
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(max_length=255)
+    password: str
+
+
 class UserRegistrationSchema(BaseModel):
     email: EmailStr = Field(max_length=255)
     password: str
