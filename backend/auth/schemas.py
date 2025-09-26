7f799b114245c63ee27aa2bb04a02f2dbb2be8b6
None None <katsuyoriiii@gmail.com>
2025-09-26 13:08:08 +0300

    Create registration user schema

---
+++ backend/auth/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
+from core.validators.password import validator_password_complexity
+
+
+class UserRegistrationSchema(BaseModel):
+    email: EmailStr = Field(max_length=255)
+    password: str
+
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, value):
+        return validator_password_complexity(value)
