a760b004a642c72fc7fe65c1e3b349d82fe37078
None None <katsuyoriiii@gmail.com>
2025-09-26 12:43:10 +0300

    Include postgres and redis, create settings and connections for db and redis

---
+++ backend/src/settings.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class CustomBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='../.env', extra='allow')
+
+
+class DatabaseSettings(CustomBaseSettings):
+    DB_NAME: str
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f'postgresql+asyncpg://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+
+class RedisSettings(CustomBaseSettings):
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_PASSWORD: str
+
+
+database_settings = DatabaseSettings()
+redis_settings = RedisSettings()
