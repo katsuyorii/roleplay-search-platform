4a17a9ef09cadfcf9326865f9530278f61c644fa
None None <katsuyoriiii@gmail.com>
2025-09-30 16:34:06 +0300

    Add repositories for tags, fandoms, fetishes and taboo, change function to create announcement

--- backend/announcements/dependencies.py
+++ backend/announcements/dependencies.py
@@ -7,11 +7,20 @@ from users.dependencies import get_current_user
 from users.models import UserModel
 
 from .services import AnnouncementsService
-from .repositories import AnnouncementsRepository
+from .repositories import AnnouncementsRepository, FandomsRepository, TagsRepository, NsfwFetishTabooRepository
 
 
-def get_announcements_repository(session: AsyncSession = Depends(get_session)) -> AnnouncementsRepository:
-    return AnnouncementsRepository(session)
+def get_fandoms_repository(session: AsyncSession = Depends(get_session)) -> FandomsRepository:
+    return FandomsRepository(session)
+
+def get_tags_repository(session: AsyncSession = Depends(get_session)) -> TagsRepository:
+    return TagsRepository(session)
+
+def get_nsfw_fetishes_taboo_repository(session: AsyncSession = Depends(get_session)) -> NsfwFetishTabooRepository:
+    return NsfwFetishTabooRepository(session)
+
+def get_announcements_repository(session: AsyncSession = Depends(get_session), fandoms_repository: FandomsRepository = Depends(get_fandoms_repository), tags_repository: TagsRepository = Depends(get_tags_repository), nsfw_fetishes_taboo_repository: NsfwFetishTabooRepository = Depends(get_nsfw_fetishes_taboo_repository)) -> AnnouncementsRepository:
+    return AnnouncementsRepository(fandoms_repository, tags_repository, nsfw_fetishes_taboo_repository, session)
 
 def get_announcements_service(announcements_repository: AnnouncementsRepository = Depends(get_announcements_repository), current_user: UserModel = Depends(get_current_user)) -> AnnouncementsService:
     return AnnouncementsService(announcements_repository, current_user)
--- backend/announcements/repositories.py
+++ backend/announcements/repositories.py
@@ -1,16 +1,58 @@
+import uuid
+
+from sqlalchemy import select, any_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.repositories.database_base import DatabaseBaseRepository
 
-from .models import AnnouncementModel
+from .models import AnnouncementModel, FandomModel, TagModel, NsfwFetishTabooModel
+
+
+class FandomsRepository(DatabaseBaseRepository):
+    def __init__(self, session: AsyncSession, model: FandomModel = FandomModel):
+        super().__init__(session, model)
+    
+    async def get_by_ids(self, ids: list[uuid.UUID]):
+        result = await self.session.execute(select(self.model).where(self.model.id.in_(ids)))
+
+        return result.scalars().all()
+
+
+class TagsRepository(FandomsRepository):
+    def __init__(self, session: AsyncSession, model: TagModel = TagModel):
+        super().__init__(session, model)
+
+
+class NsfwFetishTabooRepository(FandomsRepository):
+    def __init__(self, session: AsyncSession, model: NsfwFetishTabooModel = NsfwFetishTabooModel):
+        super().__init__(session, model)
 
 
 class AnnouncementsRepository(DatabaseBaseRepository):
-    def __init__(self, session: AsyncSession, model: AnnouncementModel = AnnouncementModel):
+    def __init__(self, fandoms_repository: FandomsRepository, tags_repository: TagsRepository, nsfw_fetishes_taboo_repository: NsfwFetishTabooRepository, session: AsyncSession, model: AnnouncementModel = AnnouncementModel):
         super().__init__(session, model)
+        self.fandoms_repository = fandoms_repository
+        self.tags_repository = tags_repository
+        self.nsfw_fetishes_taboo_repository = nsfw_fetishes_taboo_repository
 
     async def create(self, obj_dict: dict) -> AnnouncementModel:
         obj = self.model(**obj_dict)
+
+        if obj.fandoms:
+            selected_fandoms = await self.fandoms_repository.get_by_ids(obj_dict.get('fandoms'))
+            obj.fandoms = selected_fandoms
+        
+        if obj.tags:
+            selected_tags = await self.tags_repository.get_by_ids(obj_dict.get('tags'))
+            obj.tags = selected_tags
+        
+        if obj.nsfw_fetishes:
+            selected_nsfw_fetishes = await self.nsfw_fetishes_taboo_repository.get_by_ids(obj_dict.get('nsfw_fetishes'))
+            obj.nsfw_fetishes = selected_nsfw_fetishes
+        
+        if obj.nsfw_taboo:
+            selected_nsfw_taboo = await self.nsfw_fetishes_taboo_repository.get_by_ids(obj_dict.get('nsfw_taboo'))
+            obj.nsfw_taboo = selected_nsfw_taboo
 
         self.session.add(obj)
         await self.session.commit()
